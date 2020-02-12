30bc56f94b480d85500790809195bb84a19f9168
maryamAhmedShaaban <58528899+maryamAhmedShaaban@users.noreply.github.com>
2020-02-12 15:47:34 +0200

    Add files via upload

---
+++ TivaDrivers/main.c
@@ -0,0 +1,21 @@
+/*
+ * Date : 12/2/2020
+ * Author : Maryam
+ * */
+
+
+#include "dio.h"
+#include "port.h"
+#include "SysCtr.h"
+#include "dio_types.h"
+#include "processor.h"
+
+int main(void)
+{
+    /*PROCESSOR_switchMode(PROCESSOR_ModeNonPrivilege);*/
+    SysCtr_init();
+    PORT_init();
+    /*Led On*/
+    Dio_WritePort(Dio_Port_F, 0x0E);
+    return 0;
+}
